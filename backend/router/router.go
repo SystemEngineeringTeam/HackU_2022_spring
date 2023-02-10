e8edd7a5cbeb4175c5a6c953eef6ef886ed58340
Yusei Mizutani <yusei4520030401@gmail.com>
2023-02-10 10:40:01 +0900

    ginのインストール、routerの作成

---
+++ backend/router/router.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Init() {
+	r := gin.Default()
+
+	// 部屋idから部屋の詳細情報を取得
+	r.GET("/api/room/details",)
+
+	// 部屋を建てるまたは、小部屋を作成する（childrenに部屋IDを追加していく）
+	r.POST("/api/room",)
+
+	// 部屋の概要を変更
+	r.PUT("/api/room/:roomId", )
+
+	// 部屋を削除
+	r.DELETE("/api/room/:roomId/:roomKey", )
+
+	// 参加者の変更（名前、または備考欄を変更する時）
+	r.POST("/api/room/:roomId/member", )
+
+	// 部屋からの退室
+	r.PUT("/api/room/:roomId/member/:userId",)
+	r.Run()
+}
