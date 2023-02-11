8899ce7ea209283bdf94bb783bc11b955b5dc6eb
Yusei Mizutani <yusei4520030401@gmail.com>
2023-02-11 13:25:55 +0900

    部屋一覧取得のGETメソッドを追加

--- backend/router/router.go
+++ backend/router/router.go
@@ -10,23 +10,26 @@ func Init() {
 	// 部屋idから部屋の詳細情報を取得
 	r.GET("/api/room/details/")
 
+	// 参加している部屋の一覧を取得（閲覧したことのある部屋一覧を見るときに叩く）
+	r.GET("/api/room?roomId=dmlksd")
+
 	// 部屋を建てるまたは、小部屋を作成する（childrenに部屋IDを追加していく）
 	r.POST("/api/room/")
-
-	// 部屋の概要を変更
-	r.PUT("/api/room/:roomId/")
-
-	// 部屋を削除
-	r.DELETE("/api/room/:roomId/:roomKey/")
 
 	// 参加者の追加
 	r.POST("/api/room/:roomId/member/")
 
+	// 部屋の概要を変更
+	r.PUT("/api/room/:roomId/")
+
 	// 参加者の変更（名前、または備考欄を変更する時）
 	r.PUT("/api/room/:roomId/member/:userId/")
 
+	// 部屋を削除
+	r.DELETE("/api/room/:roomId/:roomKey/")
+
 	// 部屋からの退室
-	r.PUT("/api/room/:roomId/member/:userId/")
+	r.DELETE("/api/room/:roomId/member/:userId/")
 
 	r.Run()
 }
