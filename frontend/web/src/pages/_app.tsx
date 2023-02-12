20d632a85f3d445e23274a64a02bd1e33544bead
Yusei Mizutani <yusei4520030401@gmail.com>
2023-02-12 21:45:09 +0900

    RecoilRoot の設定

--- frontend/web/src/pages/_app.tsx
+++ frontend/web/src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import { HeaderLayout } from "@/components/templates/HeaderLayout";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 import theme from "../theme/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <HeaderLayout>
-        <Component {...pageProps} />
-      </HeaderLayout>
+      <RecoilRoot>
+        <HeaderLayout>
+          <Component {...pageProps} />
+        </HeaderLayout>
+      </RecoilRoot>
     </ChakraProvider>
   );
 }
