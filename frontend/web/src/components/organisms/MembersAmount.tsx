159ec44aa42f10c94ca51907c781625a26f1f454
Yusei Mizutani <yusei4520030401@gmail.com>
2023-02-14 01:19:48 +0900

    閲覧履歴のある部屋の表示

--- frontend/web/src/components/organisms/MembersAmount.tsx
+++ frontend/web/src/components/organisms/MembersAmount.tsx
@@ -1,18 +1,29 @@
+import { FC } from "react";
 import { useRecoilValue } from "recoil";
 import { HStack, Box, Text } from "@chakra-ui/react";
-import { roomDetailsState } from "../../store/roomDetailsState";
 
-export const MembersAmount = () => {
+import { roomDetailsState } from "@/store/roomDetailsState";
+
+type Props = {
+  p: string;
+  bg: string;
+  fontSize: string;
+};
+
+export const MembersAmount: FC<Props> = (props) => {
+  const { p, bg, fontSize } = props;
+
   const roomDetails = useRecoilValue(roomDetailsState);
+
   return (
     <HStack>
       <Box>
         <Text
           as="mark"
+          p={p}
           color="white"
-          backgroundColor="orange.300"
-          p={2}
-          fontSize="md"
+          bg={bg}
+          fontSize={fontSize}
           fontWeight="bold"
         >
           参加人数
