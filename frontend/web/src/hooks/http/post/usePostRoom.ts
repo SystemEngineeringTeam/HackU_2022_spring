b314b5ac64c42f653c50b4507b540046404820c7
k21066-Shimizu <k21066kk@aitech.ac.jp>
2023-02-14 00:07:31 +0900

    部屋を作成するHookを作成 #3

--- frontend/web/src/hooks/http/post/usePostRoom.ts
+++ frontend/web/src/hooks/http/post/usePostRoom.ts
@@ -1,9 +1,40 @@
+import { Room } from "@/types/room";
+import axios from "axios";
+import { useCallback, useState } from "react";
+
 type Props = {
   roomName: string;
   roomMaker: string;
-  parent: string;
+  parent?: string;
 };
 
 export const usePostRoom = () => {
-  const postRoom = (props: Props) => {};
+  const [createdRoom, setCreatedRoom] = useState<undefined | Room[]>();
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [error, setError] = useState<Error | undefined>(undefined);
+
+  const postRoom = useCallback(
+    async (props: Props) => {
+      setIsLoaded(false);
+      setIsError(false);
+
+      try {
+        const response = await axios.post<Room[]>('/api/room', {
+          roomName: props.roomName,
+          roomMaker: props.roomMaker,
+          parent: props.parent
+        });
+        setIsLoaded(true);
+        setCreatedRoom(response.data);
+      } catch (e) {
+        if (e instanceof Error) {
+          setIsError(true);
+          setError(e);
+        }
+      }
+    }, []
+  );
+
+  return { postRoom, isLoaded, allRooms: createdRoom, isError, error };
 };
