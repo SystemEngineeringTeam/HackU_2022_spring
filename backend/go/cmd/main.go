e1108f6e6c052143c57a52c34eaa40acd1180bb6
Shika-san <89015638+Bayashin@users.noreply.github.com>
2023-02-15 00:46:21 +0900

    Revert "struct で定義されてる構造体のフィールド名と init で定義されてるフィールド名の修正"

--- backend/go/cmd/main.go
+++ backend/go/cmd/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	// "github.com/SystemEngineeringTeam/Hack-U_2022/backend/go/router"
-	"fmt"
-
 	"github.com/SystemEngineeringTeam/Hack-U_2022/backend/go/lib"
 	"gorm.io/gorm"
 
@@ -11,24 +9,17 @@ import (
 )
 
 func main() {
-	//本番はrouter.Init()のみでいい
 	// router.Init()
-
-	// データベース接続確認 
-	//db connected!!　とコンソールに表示されたら接続成功
 	db := lib.SqlConnect()
-
-	// データベースの値をとれているか確認
-	// &{2 test 10 3000円以内がいい true 2022-01-03 1 田中 []} と表示されれば成功
-	// 一番最後のから配列は member テーブルの情報が入るようにしたい
-	// struct.go に for文をまわして格納する用の関数を作るのが良さそう
-	res := GetRooms(db)
-	fmt.Println(res[0])
+	print(db)
 }
 
-// テスト用の関数
-func GetRooms(db *gorm.DB) []*models.Room {
+
+func GetBuildingName(building_name string, db *gorm.DB) []*models.Room {
 	room := []*models.Room{}
-	db.Find(&room)
+	// building_nameが空文字だった時
+
+	db.Where("room_name LIKE ?", "%"+"test"+"%").Find(&room)
+
 	return room
 }
--- backend/go/models/structs.go
+++ backend/go/models/structs.go
@@ -3,20 +3,21 @@ package models
 type Room struct {
 	ID           int      `json:"roomId"`
 	RoomName     string   `json:"roomName"`
+	RoomMaker    string   `json:"roomMaker"`
+	LastUpdated  string   `json:"lastUpdated"`
 	MemberAmount int      `json:"memberAmount"`
 	Summary      string   `json:"summary"`
-	IsOpen       bool     `json:"isOpen"`
-	LastUpdate  string   `json:"lastUpdated"`
-	RoomParent   int      `json:"roomParent"`
-	RoomMaker    string   `json:"roomMaker"`
+	RoomChildren []string `json:"roomChildren"`
+	RoomParent   string   `json:"roomParent"`
 	Members      []Member `gorm:"foreignKey:RoomId"`
+	IsOpen       bool     `json:"isOpen"`
 }
 
 type Member struct {
-	ID               int    `json:"memberId"`
-	RoomId           int    `json:"roomId"`
-	UserId           int    `gorm:"primaryKey"`
-	UserName         string `json:"name"`
-	RegistrationDate string `json:"registrationDate"`
-	Comment          string `json:"comment"`
+	ID              int    `json:"memberId"`
+	RoomId          int    `json:"roomId"`
+	UserId          int    `gorm:"primaryKey"`
+	Name            string `json:"name"`
+	ResistationDate string `json:"resistationDate"`
+	Comment         string `json:"comment"`
 }
