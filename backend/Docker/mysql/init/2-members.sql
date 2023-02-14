ac4aa7065b3ea33473d7ec0961699b9fc91db0f8
Bayashin <k21099kk@aitech.ac.jp>
2023-02-14 17:08:21 +0900

    外部キーの追加

--- backend/Docker/mysql/init/2-members.sql
+++ backend/Docker/mysql/init/2-members.sql
@@ -1,9 +1,10 @@
 /* CREATE TABLE */
 CREATE TABLE data_sets.members( /* ユーザデータ */
     id INT NOT NULL AUTO_INCREMENT, /* ユーザID */
-    room_id BIGINT(20) UNSIGNED NOT NULL,
+    room_id INT NOT NULL, /* ユーザの所属している部屋ID */
     user_name VARCHAR(15), /* ユーザ名 */
     registration_date VARCHAR(15), /* 登録した日時 */
     comment VARCHAR(30), /* 備考欄 */
-    primary key (id)
-) ENGINE = InnoDB;
+    PRIMARY KEY (id),
+    FOREIGN KEY room (room_id) REFERENCES rooms (id) /* 外部キーの生成 */
+) ENGINE = InnoDB; 
