fd306f64241e78be9bf0051f46f3e8df55168fee
k21066-Shimizu <k21066kk@aitech.ac.jp>
2023-02-14 16:56:55 +0900

    メンバーの追加と変更をするHookを作製 #3

--- frontend/web/src/hooks/http/post/usePostMember.ts
+++ frontend/web/src/hooks/http/post/usePostMember.ts
@@ -1,3 +1,7 @@
+import { Room } from "@/types/room";
+import axios from "axios";
+import { useCallback, useState } from "react";
+
 type Props = {
   roomId: string;
   name: string;
@@ -5,5 +9,31 @@ type Props = {
 };
 
 export const usePostMember = () => {
-  const postMember = (props: Props) => {};
+  const [addMember, setAddMember] = useState<undefined | Room[]>();
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [error, setError] = useState<Error | undefined>(undefined);
+
+  const postMember = useCallback(
+    async (props: Props) => {
+      setIsLoaded(false);
+      setIsError(false);
+
+      try {
+        const response = await axios.post<Room[]>(`/api/room/${props.roomId}/member`, {
+          name: props.name,
+          comment: props.comment
+        });
+        setIsLoaded(true);
+        setAddMember(response.data);
+      } catch (e) {
+        if (e instanceof Error) {
+          setIsError(true);
+          setError(e);
+        }
+      }
+    }, []
+  );
+
+  return { postMember, isLoaded, addMember, isError, error };
 };
--- frontend/web/src/hooks/http/put/usePutMember.ts
+++ frontend/web/src/hooks/http/put/usePutMember.ts
@@ -1,10 +1,40 @@
+import { Room } from "@/types/room";
+import axios from "axios";
+import { useCallback, useState } from "react";
+
 type Props = {
   roomId: string;
-  userId: string;
+  memberId: string;
   name: string;
   comment: string;
 };
 
 export const usePutMember = () => {
-  const putMember = (props: Props) => {};
+  const [editMember, setEditMember] = useState<undefined | Room[]>();
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [error, setError] = useState<Error | undefined>(undefined);
+
+  const putMember = useCallback(
+    async (props: Props) => {
+      setIsLoaded(false);
+      setIsError(false);
+
+      try {
+        const response = await axios.put<Room[]>(`/api/room/${props.roomId}/member/${props.memberId}`, {
+          name: props.name,
+          comment: props.comment
+        });
+        setIsLoaded(true);
+        setEditMember(response.data);
+      } catch (e) {
+        if (e instanceof Error) {
+          setIsError(true);
+          setError(e);
+        }
+      }
+    }, []
+  );
+
+  return { putMember, isLoaded, editMember, isError, error };
 };
