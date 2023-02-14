9ec1817d0cba9ee690048aaac81eb6d368c5ce4d
OtaKeisuke <meronn0818@gmail.com>
2023-02-14 22:58:14 +0900

    メンバーに関するHTTPリクエスト部分の修正
    importにcontrollerを追加
    member_controllerにある関数を呼び出すよう修正

--- backend/go/router/router.go
+++ backend/go/router/router.go
@@ -2,13 +2,13 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"github.com/SystemEngineeringTeam/Hack-U_2022/backend/go/controller"
 )
 
 func Init() {
-	r := gin.Default()
 
-	// 部屋idから部屋の詳細情報を取得
-	r.GET("/api/room/details/")
+	r := gin.Default()
 
 	// 部屋idから部屋の詳細情報を取得
 	r.GET("/api/room/details/")
@@ -20,19 +20,19 @@ func Init() {
 	r.POST("/api/room/")
 
 	// 参加者の追加
-	r.POST("/api/room/:roomId/member/")
+	r.POST("/api/room/:roomId/member/", controller.AddMember)
 
 	// 部屋の概要を変更
 	r.PUT("/api/room/:roomId/")
 
 	// 参加者の変更（名前、または備考欄を変更する時）
-	r.PUT("/api/room/:roomId/member/:userId/")
+	r.PUT("/api/room/:roomId/member/:userId/", controller.ChangeMemberData)
 
 	// 部屋を削除
 	r.DELETE("/api/room/:roomId/:roomKey/")
 
 	// 部屋からの退室
-	r.DELETE("/api/room/:roomId/member/:userId/")
+	r.DELETE("/api/room/:roomId/member/:userId/", controller.DeleteMember)
 
 	r.Run()
 }
