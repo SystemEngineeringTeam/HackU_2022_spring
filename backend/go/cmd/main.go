f5e147b83bfe549aa1e2d040dc063b43df688f63
OtaKeisuke <meronn0818@gmail.com>
2023-02-15 19:54:16 +0900

    メンバーのダミーデータ表示用テストコードの追加

--- backend/go/cmd/main.go
+++ backend/go/cmd/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	// "github.com/SystemEngineeringTeam/Hack-U_2022/backend/go/router"
 	"fmt"
 
 	"github.com/SystemEngineeringTeam/Hack-U_2022/backend/go/lib"
+	"github.com/SystemEngineeringTeam/Hack-U_2022/backend/go/router"
 	"gorm.io/gorm"
 
 	"github.com/SystemEngineeringTeam/Hack-U_2022/backend/go/models"
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	//本番はrouter.Init()のみでいい
-	// router.Init()
+	router.Init()
 
 	// データベース接続確認
 	//db connected!!　とコンソールに表示されたら接続成功
@@ -24,6 +24,10 @@ func main() {
 	// struct.go に for文をまわして格納する用の関数を作るのが良さそう
 	res := GetRooms(db)
 	fmt.Println(res[0])
+
+	// &{1, 2, 山田太郎, 2022-01-03, 23時まで} と表示されれば成功
+	res2 := GetMembers(db)
+	fmt.Println(res2[0])
 }
 
 // テスト用の関数
@@ -31,4 +35,10 @@ func GetRooms(db *gorm.DB) []*models.Room {
 	room := []*models.Room{}
 	db.Find(&room)
 	return room
+}
+
+func GetMembers(db *gorm.DB) []*models.Member {
+	member := []*models.Member{}
+	db.Find(&member)
+	return member
 }
