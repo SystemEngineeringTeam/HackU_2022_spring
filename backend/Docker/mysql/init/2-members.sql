7dbc57824e8733c8711a228553694feca1702e11
OtaKeisuke <meronn0818@gmail.com>
2023-02-16 03:39:06 +0900

    カラムの名前変更

--- backend/Docker/mysql/init/2-members.sql
+++ backend/Docker/mysql/init/2-members.sql
@@ -2,7 +2,7 @@
 CREATE TABLE data_sets.members( /* ユーザデータ */
     id INT NOT NULL AUTO_INCREMENT, /* ユーザID */
     room_id INT NOT NULL, /* ユーザの所属している部屋ID */
-    user_name VARCHAR(15), /* ユーザ名 */
+    member_name VARCHAR(15), /* ユーザ名 */
     comment VARCHAR(30), /* 備考欄 */
     tag VARCHAR(15),
     PRIMARY KEY (id),
