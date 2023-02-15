a0c4b27d20cf6c7ea1fe8b1497039a74d9de14c7
Yusei Mizutani <yusei4520030401@gmail.com>
2023-02-16 01:12:09 +0900

    でもデータの挿入

--- frontend/web/src/store/roomDetailsState.ts
+++ frontend/web/src/store/roomDetailsState.ts
@@ -2,80 +2,119 @@ import { atom } from "recoil";
 
 import { Room } from "../types/room";
 
-export const roomDetailsState = atom<Room>({
+export const roomDetailsState = atom<Array<Room>>({
   key: "roomDetailsState",
-  default: {
-    roomId: "fd2aDsAf", // string 部屋ID
-    roomName: "成人式2次会", // string 部屋の名前
-    memberAmount: 15, // number その部屋にいる参加者の総数
-    summary: "", // string 部屋の概要
-    isOpen: true, // boolean 参加者受付中かどうか
-
-    lastUpdate: "2022-05-09T00:08:06+09:00", //string 最終更新日 ISO形式
-    roomChildren: ["fdahae", "fdjaje"], // Array<string> | null 小部屋のid配列
-    roomParent: null, // number? 親部屋のid 親がなければnull
-    members: [
-      {
-        memberId: "f282j10", // string ユーザーId
-        name: "たなか", // string ユーザーの名前
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
-      },
-      {
-        memberId: "fen23jw", // string ユーザーId
-        name: "すずき",
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "",
-      },
-      {
-        memberId: "f282j10", // string ユーザーId
-        name: "たなか", // string ユーザーの名前
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
-      },
-      {
-        memberId: "fen23jw", // string ユーザーId
-        name: "すずき",
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "",
-      },
-      {
-        memberId: "f282j10", // string ユーザーId
-        name: "たなか", // string ユーザーの名前
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
-      },
-      {
-        memberId: "fen23jw", // string ユーザーId
-        name: "すずき",
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "",
-      },
-      {
-        memberId: "f282j10", // string ユーザーId
-        name: "たなか", // string ユーザーの名前
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
-      },
-      {
-        memberId: "fen23jw", // string ユーザーId
-        name: "すずき",
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "",
-      },
-      {
-        memberId: "f282j10", // string ユーザーId
-        name: "たなか", // string ユーザーの名前
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
-      },
-      {
-        memberId: "fen23jw", // string ユーザーId
-        name: "すずき",
-        registrationDate: "2022-05-09T00:08:06+09:00", // 登録した時の時間
-        comment: "",
-      },
-    ],
-    roomMaker: "たなか", // string 部屋を作った人の名前
-  },
+  default: [
+    {
+      roomId: 1, // string 部屋ID
+      roomName: "成人式2次会", // string 部屋の名前
+      memberAmount: 15, // number その部屋にいる参加者の総数
+      summary: "カラオケ：ここにある", // string 部屋の概要
+      isOpen: true, // boolean 参加者受付中かどうか
+      lastUpdate: "2022-05-09T00:08:06+09:00", //string 最終更新日 ISO形式
+      members: [
+        {
+          memberId: 1, // string ユーザーId
+          name: "たなか", // string ユーザーの名前
+          comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
+          tag: "カラオケ",
+        },
+        {
+          memberId: 2, // string ユーザーId
+          name: "すずき",
+          comment: "",
+          tag: "居酒屋",
+        },
+        {
+          memberId: 3, // string ユーザーId
+          name: "やまだ", // string ユーザーの名前
+          comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
+          tag: "ボーリング",
+        },
+        {
+          memberId: 4, // string ユーザーId
+          name: "みずたに",
+          comment: "",
+          tag: "カラオケ",
+        },
+        {
+          memberId: 5, // string ユーザーId
+          name: "すぎやま", // string ユーザーの名前
+          comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
+          tag: "カラオケ",
+        },
+        {
+          memberId: 6, // string ユーザーId
+          name: "はっとり",
+          comment: "",
+          tag: "居酒屋",
+        },
+        {
+          memberId: 7, // string ユーザーId
+          name: "さとう", // string ユーザーの名前
+          comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
+          tag: "カラオケ",
+        },
+        {
+          memberId: 8, // string ユーザーId
+          name: "なるせ",
+          comment: "",
+          tag: "ボーリング",
+        },
+        {
+          memberId: 9, // string ユーザーId
+          name: "みつや", // string ユーザーの名前
+          comment: "3000円以内のお店がいいです", // string 備考欄（使わないかも）
+          tag: "居酒屋",
+        },
+        {
+          memberId: 10, // string ユーザーId
+          name: "さかもと",
+          comment: "",
+          tag: "カラオケ",
+        },
+      ],
+      roomMaker: "たなか", // string 部屋を作った人の名前}
+    },
+    {
+      roomId: 2, // string 部屋ID
+      roomName: "成人式2次会", // string 部屋の名前
+      memberAmount: 15, // number その部屋にいる参加者の総数
+      summary: "カラオケ：ここにある", // string 部屋の概要
+      isOpen: true, // boolean 参加者受付中かどうか
+      lastUpdate: "2022-05-09T00:08:06+09:00", //string 最終更新日 ISO形式
+      members: [],
+      roomMaker: "たなか", // string 部屋を作った人の名前}
+    },
+    {
+      roomId: 3, // string 部屋ID
+      roomName: "成人式2次会", // string 部屋の名前
+      memberAmount: 15, // number その部屋にいる参加者の総数
+      summary: "カラオケ：ここにある", // string 部屋の概要
+      isOpen: true, // boolean 参加者受付中かどうか
+      lastUpdate: "2022-05-09T00:08:06+09:00", //string 最終更新日 ISO形式
+      members: [],
+      roomMaker: "たなか", // string 部屋を作った人の名前}
+    },
+    {
+      roomId: 4, // string 部屋ID
+      roomName: "成人式2次会", // string 部屋の名前
+      memberAmount: 15, // number その部屋にいる参加者の総数
+      summary: "カラオケ：ここにある", // string 部屋の概要
+      isOpen: true, // boolean 参加者受付中かどうか
+      lastUpdate: "2022-05-09T00:08:06+09:00", //string 最終更新日 ISO形式
+      members: [],
+      roomMaker: "たなか", // string 部屋を作った人の名前}
+    },
+    {
+      roomId: 5, // string 部屋ID
+      roomName: "成人式2次会", // string 部屋の名前
+      memberAmount: 15, // number その部屋にいる参加者の総数
+      summary: "カラオケ：ここにある", // string 部屋の概要
+      isOpen: true, // boolean 参加者受付中かどうか
+      lastUpdate: "2022-05-09T00:08:06+09:00", //string 最終更新日 ISO形式
+      members: [],
+      roomMaker: "たなか", // string 部屋を作った人の名前}
+    },
+  ],
 });
