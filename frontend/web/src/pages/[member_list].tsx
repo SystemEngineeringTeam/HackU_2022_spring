167cf43d62d7cc61513a3810a9e14a9727bb32aa
Yusei Mizutani <yusei4520030401@gmail.com>
2023-02-16 13:45:40 +0900

    参加フォームドロワーの実装

--- frontend/web/src/pages/[member_list].tsx
+++ frontend/web/src/pages/[member_list].tsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
-import { Box, Divider, Heading, Text, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Heading,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 import { roomsState } from "@/store/roomDetailsState";
 import { Summary } from "@/components/organisms/Summary";
 import { MembersAmount } from "@/components/organisms/MembersAmount";
 import { ModalAddMenber } from "@/components/molecules/modal/ModalAddMenber";
 import { FixedBottomButtons } from "@/components/organisms/FixedBottomButtons";
+import { NameAndCommentForm } from "../components/organisms/NameAndCommentForm";
 import { ModalAddSmallRoom } from "@/components/molecules/modal/ModalAddSmallRoom";
 import { TabsAllMemberOrSmallRooms } from "@/components/organisms/TabsAllMemberOrSmallRooms";
 
@@ -14,11 +23,15 @@ export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const [isModalAddMenberOpen, setIsModalAddMenber] = useState(false);
+  const [isDrawerMenberFormOpen, setIsDrawerMenberFormOpen] = useState(false);
 
   const [rooms, setRooms] = useRecoilState(roomsState);
 
   const onModalAddMenberOpen = () => setIsModalAddMenber(true);
   const onModalAddMenberClose = () => setIsModalAddMenber(false);
+
+  const onDrawerMenberFormOpen = () => setIsDrawerMenberFormOpen(true);
+  const onDrawerMenberFormClose = () => setIsDrawerMenberFormOpen(false);
 
   useEffect(() => {
     // GET リクエストを送ってグローバルステイトで管理しているroomsにデータを格納する
@@ -40,7 +53,7 @@ export default function Home() {
       />
       <Box p={4}>
         <Text fontSize="2xl" fontWeight="bold" whiteSpace="unset">
-          部屋のタイトル
+          {rooms[0].roomName}
         </Text>
       </Box>
       <Divider borderColor="gray.400" />
@@ -58,6 +71,31 @@ export default function Home() {
         </Box>
       </Box>
       <TabsAllMemberOrSmallRooms />
+
+      <Center>
+        <Button
+          type="submit"
+          w="100px"
+          h="100px"
+          rounded="full"
+          color="white"
+          backgroundColor="orange.400"
+          mt={4}
+          mb={8}
+          _hover={{ bg: "orange.500" }}
+          _active={{ bg: "orange.600" }}
+          onClick={onDrawerMenberFormOpen}
+        >
+          参加
+          <br />
+          入力する
+        </Button>
+      </Center>
+
+      <NameAndCommentForm
+        isOpen={isDrawerMenberFormOpen}
+        onClose={onDrawerMenberFormClose}
+      />
     </>
   );
 }
