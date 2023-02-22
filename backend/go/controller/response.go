4fc354de7bdc032710d467487197a5fea148147c
OtaKeisuke <meronn0818@gmail.com>
2023-02-23 03:25:51 +0900

    RoomChangeの引数変更に伴うコード修正

--- backend/go/controller/response.go
+++ backend/go/controller/response.go
@@ -40,7 +40,9 @@ func ResponseChangeRoom(c *gin.Context) {
 		return
 	}
 	id := c.Param("roomId")
-	c.JSON(http.StatusOK, RoomChange(id, req))
+	defer ErrorResponse(c)
+	room := RoomGet(id)
+	c.JSON(http.StatusOK, RoomChange(req, room))
 }
 
 func ResponseDeleteRoom(c *gin.Context) {
